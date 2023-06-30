d8b3c6a93ec8fc32ee75a91aee41760b8634dff5
Josh XT <josh@devxt.com>
2023-06-30 18:52:02 -0400

    v0.0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(this_directory, "requirements.txt")) as f:
 
 setup(
     name="drmmsdk",
-    version="0.0.1",
+    version="0.0.2",
     description="Datto RMM Unofficial Python SDK",
     long_description=long_description,
     long_description_content_type="text/markdown",
